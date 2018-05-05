d45c2e3135ff8fb6d10fd32b9b67d74192e41ffd
mkMakz <33726337+mkMakz@users.noreply.github.com>
2018-05-05 15:37:10 +0600

    Add project files.

---
+++ IndexersLite/ArrayForIndexer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersLite
+{
+    public class ArrayForIndexer
+    {
+        private Random rand = new Random();
+
+        public int[] arr = new int[10] { 10, 3, 6, 1, 2, 5, 7, 4, 3, 12};
+
+        public int this[int ind]
+        {
+
+            get
+            {
+                return arr[ind];
+            }
+            set
+            {
+                arr[ind] = (int)Math.Pow(value, 2);
+            }
+        }
+
+        
+
+
+
+    }
+}
---
+++ IndexersLite/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayForIndexer arrayForIndexer = new ArrayForIndexer();
+
+            for (int i = 0; i < arrayForIndexer.arr.Length; i++)
+            {
+                Console.WriteLine($"Index {i} Value {arrayForIndexer.arr[i]}");
+            }
+
+            Console.WriteLine("\n");
+
+            arrayForIndexer[0] = 5;
+            arrayForIndexer[1] = 4;
+            arrayForIndexer[2] = 10;
+            arrayForIndexer[3] = 3;
+
+            for (int i = 0; i < arrayForIndexer.arr.Length; i++)
+            {
+                Console.WriteLine($"Index {i} Value {arrayForIndexer.arr[i]}");
+            }
+
+
+        }
+    }
+}
